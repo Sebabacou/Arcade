e0e65768907de07c8ec5c57ab43e9caa4fe122e1
root <arthur.doriel@epitech.eu>
2024-03-25 15:07:24 +0100

    feat(Arcade): Libraries

--- src/common/Event.hpp
+++ src/common/Event.hpp
@@ -38,5 +38,8 @@ namespace Arcade {
         MENU, ///< Represents the 'MENU' key
         GAME_SHOOT,
         NONE, ///< Represents no key
+
+        IN_A, IN_B, IN_C, IN_D, IN_E, IN_F, IN_G, IN_H, IN_I, IN_J, IN_K, IN_L, IN_M, IN_N, IN_O, IN_P, IN_Q, IN_R, IN_S, IN_T, IN_U, IN_V, IN_W, IN_X, IN_Y, IN_Z,
+        IN_1, IN_2, IN_3, IN_4, IN_5, IN_6, IN_7, IN_8, IN_9
     };
 }
