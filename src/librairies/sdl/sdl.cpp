9003ac372441d8262b987b842e3886de00682c05
root <arthur.doriel@epitech.eu>
2024-04-06 15:07:23 +0200

    Fix(Arcade): last feature

--- src/librairies/sdl/sdl.cpp
+++ src/librairies/sdl/sdl.cpp
@@ -145,7 +145,7 @@ namespace Arcade {
         rect.w = SIZE;
         rect.h = SIZE;
 
-        if (object->getAsset().empty()  && IMG_LoadTexture(_renderer, object->getAsset().c_str()) != NULL) {
+        if (object->getAsset().empty() && IMG_LoadTexture(_renderer, object->getAsset().c_str()) != NULL) {
             SDL_RenderCopy(_renderer, _textures[object->getAsset()], NULL, &rect);
         } else {
             Colors color = object->getColor();
